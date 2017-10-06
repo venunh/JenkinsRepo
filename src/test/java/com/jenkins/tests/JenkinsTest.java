d25ef9a26557f7a73563f2cb39b63d2f6b3ea44c
212585788 <>
2017-10-06 16:06:14 +0530

    Test Jenkins

---
+++ src/test/java/com/jenkins/tests/JenkinsTest.java
@@ -0,0 +1,13 @@
+package com.jenkins.tests;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	public void printMessage()
+	{
+		System.out.println("Testing Jenkins configuration");
+	}
+
+}
