77d3938f4607105a2337a2f1107baf2d7309d808
212585788 <212585788@G3YB7JC2E.logon.ds.ge.com>
2017-10-09 14:57:04 +0530

    New driver server IE

--- src/test/java/com/jenkins/tests/JenkinsTest.java
+++ src/test/java/com/jenkins/tests/JenkinsTest.java
@@ -1,5 +1,7 @@
 package com.jenkins.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class JenkinsTest {
@@ -8,6 +10,9 @@ public class JenkinsTest {
 	public void printMessage()
 	{
 		System.out.println("Testing Jenkins configuration..");
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/src/test/resources/IEDriverServer/IEDriverServer.exe");
+		WebDriver driver = new InternetExplorerDriver();
+		driver.get("https://www.google.com");
 	}
 
 }
