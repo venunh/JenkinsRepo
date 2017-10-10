7c7a416b7165df28bef8a314bd0331f4de2024ab
212585788 <212585788@G3YB7JC2E.logon.ds.ge.com>
2017-10-10 17:23:17 +0530

    ie sauce

--- src/test/java/com/jenkins/tests/JenkinsTest.java
+++ src/test/java/com/jenkins/tests/JenkinsTest.java
@@ -1,23 +1,31 @@
 package com.jenkins.tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class JenkinsTest {
 	
+	public static final String USERNAME = "venu";
+	  public static final String ACCESS_KEY = "14Feb1989#";
+	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	
 	@Test
-	public void printMessage()
+	public void printMessage() throws MalformedURLException
 	{
 		DesiredCapabilities desiredCapabilities = DesiredCapabilities.internetExplorer();
 		desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
 		desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
 		desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
 		System.out.println("Testing Jenkins configuration..");
-		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/src/test/resources/IEDriverServer/IEDriverServer.exe");
-		WebDriver driver = new InternetExplorerDriver();
+		//System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/src/test/resources/IEDriverServer/IEDriverServer.exe");
+		WebDriver driver = new RemoteWebDriver(new URL(URL), desiredCapabilities);
+		//WebDriver driver = new InternetExplorerDriver();
 		driver.get("https://www.google.com");
 		driver.quit();
 	}
