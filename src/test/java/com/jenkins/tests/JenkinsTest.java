26365833c50822acd0e3f3ab74c1a88443c5456b
212585788 <212585788@G3YB7JC2E.logon.ds.ge.com>
2017-10-10 17:13:08 +0530

    sauce labs

--- src/test/java/com/jenkins/tests/JenkinsTest.java
+++ src/test/java/com/jenkins/tests/JenkinsTest.java
@@ -2,6 +2,8 @@ package com.jenkins.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class JenkinsTest {
@@ -9,6 +11,10 @@ public class JenkinsTest {
 	@Test
 	public void printMessage()
 	{
+		DesiredCapabilities desiredCapabilities = DesiredCapabilities.internetExplorer();
+		desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+		desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
+		desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
 		System.out.println("Testing Jenkins configuration..");
 		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"/src/test/resources/IEDriverServer/IEDriverServer.exe");
 		WebDriver driver = new InternetExplorerDriver();
